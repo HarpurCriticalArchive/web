70484463cada9a74daa77cfcc9937dae3379bace
chasharpur <desmond@gs709-1305.eait.uq.edu.au>
2015-07-13 12:02:59 +1000

    updated custom.sh script

--- custom.sh
+++ custom.sh
@@ -1,13 +1,22 @@
+# name of directory within /images on the webserver where 
+# images are stored, and where the html files are stored here
 htmldir=about
+# the user name whose password should be entered in .pgpass of the #
+#user's account on the server e.g. in /home/desmond/.pgpass if I use 
+#this script as the user "desmond" .pgpass looks like: 
+# *:*:*:postgres:jabberw0cky
 dbuser=postgres
+# the name of the postgres database where Drupal stores its stuff
 dbname=drupaldb
+# the drupal root directory you want to update
 webroot=/var/www/harpur
 os=`uname`
 remotesite=charles-harpur.org
 # these are the files in the same directory as this script (update.sh)
+# you can add new ones in quotes here. Pay attention to the order. nids MUST be provided (see below).
 files=("about.md" "overview.md" "editorial-policy.md" "biography.md" "permissions.md" "technical-design.md" "challenges.md" "sources.md" "poems-in-anthologies.md")
-# these are the Drupal node ids for the files (same order)
-# you can get them from drupal by going to that opage and noting down 
+# these are the Drupal node ids ("nids") for the files (same order)
+# you can get them from drupal by going to that page and noting down 
 # the nid number immediately after "node" in the url bar
 # the nids must be in the same order as the file list and the two 
 # lists have to be the same length
