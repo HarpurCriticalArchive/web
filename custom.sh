88dc8ec8999e3448052f1dfd8c2de885104acc31
chasharpur <desmond@d-i184-21-147.staff.eduroam.uq.edu.au>
2015-07-22 10:34:50 +1000

    added home.md to set

--- custom.sh
+++ custom.sh
@@ -16,11 +16,11 @@ os=`uname`
 remotesite=charles-harpur.org
 # these are the files in the same directory as this script (update.sh)
 # you can add new ones in quotes here. Pay attention to the order. nids MUST be provided (see below).
-files=("about.md" "overview.md" "editorial-policy.md" "biography.md" "permissions.md" "technical-design.md" "challenges.md" "sources.md" "poems-in-anthologies.md")
+files=("about.md" "overview.md" "editorial-policy.md" "biography.md" "permissions.md" "technical-design.md" "challenges.md" "sources.md" "poems-in-anthologies.md" "home.md")
 # these are the Drupal node ids ("nids") for the files (same order)
 # you can get them from drupal by going to that page and noting down 
 # the nid number immediately after "node" in the url bar
 # the nids must be in the same order as the file list and the two 
 # lists have to be the same length
-nids=(30 31 32 33 34 35 36 37 38)
+nids=(30 31 32 33 34 35 36 37 38 39)
 
